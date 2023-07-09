f0b5ff77c02fcd4d962faeaa2e70bdffbe83fe54
Iam-abhi121 <abhijeetpatil572000@gmail.com>
2023-07-09 19:56:12 +0530

    update config file

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ PORT_NUMBER = 5004
 
 MONGODB_PORT_NUMBER = 27017
 MONGODB_DATABASE_NAME = 'project_db'
-MODEL_FILE_PATH = r'static\model\linear_regression.pkl'
+MODEL_FILE_PATH = os.path.join(r'static\model\linear_regression.pkl')
 
-JSON_FILE_PATH = r'static\json_files\proj_data.json'
+JSON_FILE_PATH = os.path.join(r'static\json_files\proj_data.json')
